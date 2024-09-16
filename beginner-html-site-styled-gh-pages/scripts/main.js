fdbb3011e93304d492d7253ec619ffde54b585a4
fidelis182 <fedymueni18@gmail.com>
2024-09-16 16:29:13 +0300

    new file

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,2 +1,19 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent= "Hello World !";
+// let iceCream = "chocolate";
+// if (iceCream === "chocolate") {
+//   alert("Yay, I love chocolate ice cream!");
+// } else {
+//   alert("Awwww, but chocolate is my favorite…");
+// }
+
+function checkData() {
+  if (document.form1.threeChar.value.length === 3) {
+    return true;
+  } else {
+    alert(
+      `Enter exactly three characters. ${document.form1.threeChar.value} is not valid.`,
+    );
+    return false;
+  }
+}
